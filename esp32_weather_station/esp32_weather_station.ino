df616bab5eff470d0c9c82257fc717ac91a8c65d
amrlnic <amoriello.nicola97@gmail.com>
2021-10-15 00:05:07 +0200

    upload report
    Co-Authored-By: AndreaPolicarpi <68959695+AndreaPolicarpi@users.noreply.github.com>

--- esp32_weather_station/esp32_weather_station.ino
+++ esp32_weather_station/esp32_weather_station.ino
@@ -187,9 +187,6 @@ float readDHTTemperature() {
   // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
   // Read temperature as Celsius (the default)
   float t = dht.readTemperature();
-  // Read temperature as Fahrenheit (isFahrenheit = true)
-  //float t = dht.readTemperature(true);
-  // Check if any reads failed and exit early (to try again).
   if (isnan(t)) {
     Serial.println("Failed to read from DHT sensor!");
     return 0.0;
@@ -208,7 +205,6 @@ float readDHTHumidity() {
     return 0.0;
   }
   else {
-    //Serial.println(h);
     return h;
   }
 }
